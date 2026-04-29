8f3094e612df22223d9425413dc72493ef049791
William Perez Muñoz <90478495+williamppmm@users.noreply.github.com>
2026-04-28 19:27:55 -0500

    Fix api/index.js: build Express app from middlewares for Vercel

--- api/index.js
+++ api/index.js
@@ -1,2 +1,11 @@
-const app = require('../index');
+require('dotenv').config({ silent: true });
+const express = require('express');
+const middleWarez = require('../index.js');
+
+const app = express();
+app.get('/auth', middleWarez.auth);
+app.get('/callback', middleWarez.callback);
+app.get('/success', middleWarez.success);
+app.get('/', middleWarez.index);
+
 module.exports = app;
